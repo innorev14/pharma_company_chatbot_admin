32d2cf97a138dd8315714c2958981ab6600cf03f
innorev14 <tokyo3@netsgo.com>
2021-03-23 16:45:05 +0900

    fix/ add static_root

--- config/settings/base.py
+++ config/settings/base.py
@@ -143,6 +143,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
