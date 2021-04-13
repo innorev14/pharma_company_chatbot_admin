1e5278500aed8a77022b6d35f1b183ae2e54c8c5
innorev14 <tokyo3@netsgo.com>
2021-04-13 16:01:28 +0900

    fix: 테스트 url 수정

--- kakao/urls.py
+++ kakao/urls.py
@@ -4,6 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('keyboard/', keyboard, name='keyboard'),
-    path('keyboard2/', keyboard, name='keyboard2'),
+    path('keyboard2/', keyboard2, name='keyboard2'),
     path('message/', medicine, name='message')
 ]
