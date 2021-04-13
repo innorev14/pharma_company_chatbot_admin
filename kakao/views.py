b0e1538dca0248802272295e7880e2a349f57697
innorev14 <tokyo3@netsgo.com>
2021-04-13 15:15:56 +0900

    fix: 테스트2 수정

--- kakao/views.py
+++ kakao/views.py
@@ -12,7 +12,27 @@ def keyboard(request):
     return JsonResponse({'type': 'text'})
 
 def keyboard2(request):
-    return JsonResponse({'type': json.loads(request.body.decode('utf-8'))})
+    answer = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(answer)
+    return_str = return_json_str['userRequest']['utterance']
+
+    if return_str == '테스트':
+        return JsonResponse({
+            'version': "2.0",
+            'template': {
+                'outputs': [{
+                    'simpleText': {
+                        'text': "테스트 성공입니다."
+                    }
+                }],
+                'quickReplies': [{
+                    'label': '처음으로',
+                    'action': 'message',
+                    'messageText': '처음으로'
+                }]
+            }
+        })
+    # return JsonResponse({'type': json.loads(request.body.decode('utf-8'))})
 
 @csrf_exempt
 def medicine(request):
