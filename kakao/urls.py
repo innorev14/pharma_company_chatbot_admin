d90d110e24007db453b9a9beaf50edee75adc29a
innorev14 <tokyo3@netsgo.com>
2021-04-14 17:16:33 +0900

    feat:medicine 샘플 추가

--- kakao/urls.py
+++ kakao/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('keyboard/', keyboard, name='keyboard'),
     path('keyboard2/', keyboard2, name='keyboard2'),
-    path('message/', medicine, name='message')
+    path('medicine/', medicine, name='medicine')
 ]
--- kakao/views.py
+++ kakao/views.py
@@ -49,93 +49,33 @@ def keyboard2(request):
 
 @csrf_exempt
 def medicine(request):
-    answer = request.body.decode('utf-8')
-    return_json_str = json.loads(answer)  # json 포맷으로 카카오톡 내용 받아오기
-    return_str = return_json_str['userRequest']['utterance']  # 사용자 발화 추출
-    idx = return_str.index('을')
-    disease = return_str[:idx]
-    print(return_str)
-    print(idx)
-    print(disease)
-    return JsonResponse({'return_str': return_str,
-                         'idx': idx,
-                         'disease': disease})
-    HttpResponse
-    # manu = '제조사: '
-    # effect = '효능효과 알아보기'
-    # usage = '복용법 알아보기'
-    # precautions = '주의사항 알아보기'
-    # if return_str == disease + '을 위한 약품':  # 사용자 발화 예상
-    #     can = medicine.objects.filter(disease=disease)  # 장고와 연동된 MySQL에서 데이터 가져오기
-    #     return JsonResponse({
-    #         "version": "2.0",
-    #         "template": {
-    #             "outputs": [{
-    #                 "carousel": {  # 스킬가이드에 나온 캐러셀 형태
-    #                     "type": "basicCard",  # 기본형 선택 (<->비즈니스형도 존재)
-    #                     "items": [{
-    #                         "title": can[0].medicine,  # 제목
-    #                         "description": manu + can[0].manufacturer,  # 설명
-    #                         "thumbnail": {  # 썸네일 이미지
-    #                             "imageUrl": can[0].imgurl
-    #                         }, "buttons": [  # 버튼 {
-    #                             "action": "message",  # 동작 형태(텍스트 출력)
-    #                                       "label": "효능효과",  # 버튼 이름
-    #     "messageText": can[0].medicine + ' ' + effect
-    #     },
-    #     {
-    #         "action": "message",
-    #         "label": "복용법",
-    #         "messageText": can[0].medicine + ' ' + usage},
-    #     {
-    #         "action": "message",
-    #         "label": "주의사항",
-    #         "messageText": can[0].medicine + ' ' + precautions
-    #     }
-    #     ]
-    #     },
-    #     {"title": can[1].medicine,
-    #      "description": manu + can[1].manufacturer,
-    #      "thumbnail": {"imageUrl": can[1].imgurl},
-    #      "buttons": [{
-    #          "action": "message",
-    #          "label": "효능효과",
-    #          "messageText": can[1].medicine + ' ' + effect
-    #      },
-    #          {
-    #              "action": "message",
-    #              "label": "복용법",
-    #              "messageText": can[1].medicine + ' ' + usage
-    #          },
-    #          {
-    #              "action": "message",
-    #              "label": "주의사항",
-    #              "messageText": can[1].medicine + ' ' + precautions
-    #          }
-    #      ]
-    #      }, {
-    #         "title": can[2].medicine,
-    #         "description": manu + can[2].manufacturer,
-    #         "thumbnail": {"imageUrl": can[2].imgurl},
-    #         "buttons": [{
-    #             "action": "message",
-    #             "label": "효능효과",
-    #             "messageText": can[2].medicine + ' ' + effect
-    #         },
-    #             {
-    #                 "action": "message",
-    #                 "label": "복용법",
-    #                 "messageText": can[2].medicine + ' ' + usage
-    #             },
-    #             {
-    #                 "action": "message",
-    #                 "label": "주의사항",
-    #                 "messageText": can[2].medicine + ' ' + precautions
-    #             }
-    #         ]
-    #     ]
-    #     }
-    #     }
-    #     ]
-    #     }
-    #     })
+    user_req = request.body.decode('utf-8')
+    json_req = json.loads(user_req)
+    user_input = json_req['userRequest']['utterance']  # 유저 발화
+    user_id = json_req['userRequest']['user']['id']  # 유저 ID
+
+    # 유저 확인 로직
+
+    # 제품 정보 확인
+    medicine_info = Medicine.objects.filter(name=user_input)
+
+    text = {
+        'version': "2.0",
+        'template': {
+            'outputs': [{
+                'simpleText': {
+                    'text': medicine_info.product_info
+                }
+            }],
+            'quickReplies': [{
+                'label': '처음으로',
+                'action': 'message',
+                'messageText': '처음으로'
+            }]
+        }
+    }
+
+    return JsonResponse(text, status=200)
+
+
+
