6f4b5dc565b6c35f00befbd688760afc39578cbc
innorev14 <tokyo3@netsgo.com>
2021-04-23 22:08:07 +0900

    feat: 카카오 스킬 응답추가

--- kakao/views.py
+++ kakao/views.py
@@ -1,3 +1,7 @@
+import re
+from urllib import parse
+
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
 from django.shortcuts import render
@@ -7,6 +11,7 @@ import json
 
 from django.views.decorators.http import require_POST
 
+from accounts.models import User
 from medicine.models import *
 
 
@@ -48,43 +53,278 @@ def keyboard2(request):
             return HttpResponse(status=403)
 
 
+@require_POST
 @csrf_exempt
-def medicine(request):
-    user_req = request.body.decode('utf-8')
-    json_req = json.loads(user_req)
-    user_input = json_req['userRequest']['utterance'][:-1]  # 유저 발화
-    user_id = json_req['userRequest']['user']['id']  # 유저 ID
-
-    # 유저 확인 로직
-
-    # 제품 정보 확인
-    medicine_info = Medicine.objects.get(name=user_input)
-    medicine_name = medicine_info.name.replace(' ', '')
-
-    text = {
+def welcome(request):
+    res = {
         'version': "2.0",
         'template': {
             'outputs': [
                 {
                     "simpleImage": {
-                        "imageUrl": "https://ilhwa-pharm.s3.ap-northeast-2.amazonaws.com/" + medicine_name + ".jpg",
-                        "altText": "제품이미지"
+                        "imageUrl": "https://cdn.imweb.me/thumbnail/20171207/5a28873a44a07.png",
+                        "altText": "일화로고"
                     },
+                    "description": "안녕하세요. 일화제약 제품 안내 챗봇입니다. 최초 인증을 진행해주세요.",
                     "buttons": [
                         {
-                            "action": "message",
-                            "label": "열어보기",
-                            "messageText": "짜잔! 우리가 찾던 보물입니다"
+                            "action": "block",
+                            "label": "인증하기",
+                            "blockId": ""
                         },
-                        {
-                            "action": "webLink",
-                            "label": "구경하기",
-                            "webLinkUrl": "https://e.kakao.com/t/hello-ryan"
-                        }
                     ]
                 }
             ]
         }
     }
 
-    return JsonResponse(text, status=200)
+    return JsonResponse(res, status=200)
+
+
+@require_POST
+@csrf_exempt
+def validation(request):
+    req_kakao = request.body.decode('utf-8')
+    json_req = json.loads(user_req)
+    phone = req_kakao['value']['origin']
+    vali_num = r"^\d{10,11}$"
+    vali_num2 = r"^\d{3}-\d{3,4}-\d{4}$"
+    vali_num3 = r"^\d{3}.\d{3,4}.\d{4}$"
+    if re.match(vali_num, phone):
+        send_msg = {
+            "status": "SUCCESS",
+            "value": phone,
+        }
+    elif re.match(vali_num2, phone):
+        send_msg = {
+                    "status": "SUCCESS",
+                    "value": phone.replace('-', ''),
+        }
+    elif re.match(vali_num2, phone):
+        send_msg = {
+                    "status": "SUCCESS",
+                    "value": phone.replace('.', ''),
+        }
+    else:
+        send_msg = {
+                    "status": "FAIL",
+                    "value": phone,
+
+                    "message": "전화번호가 형식이 올바르지 않습니다. \n\n"
+        }
+
+    return send_msg
+
+
+@require_POST
+@csrf_exempt
+def auth(request):
+    user_req = request.body.decode('utf-8')
+    json_req = json.loads(user_req)
+    user_id = json_req['userRequest']['user']['id']  # 유저 ID
+    user_phone = json_req['contexts'][0]['params']['phone']['value']
+
+    user = get_user_model()
+
+    try:
+        match_user = user.objects.get(phone=user_phone)
+        if match_user.kakao_id == '':
+            match_user.kakao_id = user_id
+            match_user.save()
+        else:
+            send_msg = {}
+    except user.DoesNotExist:
+        text = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": "관리자에게 문의바랍니다."
+                        }
+                    }
+                ]
+            }
+        }
+        return JsonResponse(text, status=404)
+
+
+@require_POST
+@csrf_exempt
+def medicine(request):
+    user_req = request.body.decode('utf-8')
+    json_req = json.loads(user_req)
+    # user_input = json_req['userRequest']['utterance'][:-1]  # 유저 발화
+    user_input = json_req['action']['params']['product_name']
+    user_id = json_req['userRequest']['user']['id']  # 유저 ID
+
+    # 유저 확인 로직
+    check_id = User.objects.get(kakao_id=user_id)
+    if check_id.exists() and check_id.is_active == 1:
+        # 제품 정보 확인
+        medicine_info = Medicine.objects.get(name=user_input)
+        medicine_name = medicine_info.name.replace(' ', '')
+
+        text = {
+            'version': "2.0",
+            'template': {
+                'outputs': [
+                    {
+                        "simpleImage": {
+                            "imageUrl": "https://ilhwa-pharm.s3.ap-northeast-2.amazonaws.com/"
+                                        + parse.quote(medicine_name) + ".jpg",
+                            "altText": "제품이미지"
+                        },
+                        "buttons": [
+                            {
+                                "action": "block",
+                                "label": "제품정보",
+                                "blockId": "607e7831f1a09324e4b37a19"
+                            },
+                            {
+                                "action": "block",
+                                "label": "보험정보",
+                                "blockId": "6007a3be70fd446fa256b643"
+                            },
+                            {
+                                "action": "block",
+                                "label": "디테일 포인트",
+                                "blockId": "6007a3c83d34416490cf7ba7"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+
+        return JsonResponse(text, status=200)
+    else:
+        return JsonResponse(status=404)
+
+
+@require_POST
+@csrf_exempt
+def prod_info(request):
+    user_req = request.body.decode('utf-8')
+    json_req = json.loads(user_req)
+    # user_input = json_req['userRequest']['utterance'][:-1]  # 유저 발화
+    user_input = json_req['contexts'][0]['params']['product_name']['value']
+    user_id = json_req['userRequest']['user']['id']  # 유저 ID
+
+    # 유저 확인 로직
+    check_id = User.objects.get(kakao_id=user_id)
+    if check_id.exists() and check_id.is_active == 1:
+        # 제품 정보 확인
+        medicine_info = Medicine.objects.get(name=user_input)
+        medicine_name = medicine_info.name.replace(' ', '')
+
+        res = {
+            'version': "2.0",
+            'template': {
+                'outputs': [
+                    {
+                        "basicCard": {
+                            "thumbnail": {
+                                "imageUrl": "https://ilhwa-pharm.s3.ap-northeast-2.amazonaws.com/image/"
+                                            + parse.quote(medicine_info) + ".jpg",
+                            },
+                            "description": medicine_info.product_info.replace("<p>", "\n"),
+                            "buttons": [
+                                {
+                                    "action": "webLink",
+                                    "label": "상세보기",
+                                    "webLinkUrl": medicine_info.product_url
+                                },
+                            ]
+                        },
+                    }
+                ]
+            }
+        }
+
+        return JsonResponse(res, status=200)
+    else:
+        return JsonResponse(status=404)
+
+
+@require_POST
+@csrf_exempt
+def insu_info(request):
+    user_req = request.body.decode('utf-8')
+    json_req = json.loads(user_req)
+    # user_input = json_req['userRequest']['utterance'][:-1]  # 유저 발화
+    user_input = json_req['contexts'][0]['params']['product_name']['value']
+    user_id = json_req['userRequest']['user']['id']  # 유저 ID
+
+    # 유저 확인 로직
+    check_id = User.objects.get(kakao_id=user_id)
+    if check_id.exists() and check_id.is_active == 1:
+        # 제품 정보 확인
+        medicine_info = Medicine.objects.get(name=user_input)
+        medicine_name = medicine_info.name.replace(' ', '')
+
+        res = {
+            'version': "2.0",
+            'template': {
+                'outputs': [
+                    {
+                        "basicCard": {
+                            "thumbnail": {
+                                "imageUrl": "https://ilhwa-pharm.s3.ap-northeast-2.amazonaws.com/image/"
+                                            + parse.quote(medicine_info) + ".jpg",
+                            },
+                            "description": medicine_info.insurance_info.replace("<p>", "\n"),
+                        }
+                    }
+                ]
+            }
+        }
+
+        return JsonResponse(res, status=200)
+    else:
+        return JsonResponse(status=404)
+
+
+@require_POST
+@csrf_exempt
+def detail_point(request):
+    user_req = request.body.decode('utf-8')
+    json_req = json.loads(user_req)
+    # user_input = json_req['userRequest']['utterance'][:-1]  # 유저 발화
+    user_input = json_req['contexts'][0]['params']['product_name']['value']
+    user_id = json_req['userRequest']['user']['id']  # 유저 ID
+
+    # 유저 확인 로직
+    check_id = User.objects.get(kakao_id=user_id)
+    if check_id.exists() and check_id.is_active == 1:
+        # 제품 정보 확인
+        medicine_info = Medicine.objects.get(name=user_input)
+        medicine_name = medicine_info.name.replace(' ', '')
+
+        res = {
+            'version': "2.0",
+            'template': {
+                'outputs': [
+                    {
+                        "basicCard": {
+                            "thumbnail": {
+                                "imageUrl": "https://ilhwa-pharm.s3.ap-northeast-2.amazonaws.com/image/"
+                                            + parse.quote(medicine_info) + ".jpg",
+                            },
+                            "description": medicine_info.detail_info.replace("<p>", "\n"),
+                            "buttons": [
+                                {
+                                    "action": "webLink",
+                                    "label": "상세보기",
+                                    "webLinkUrl": medicine_info.detail_url
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+
+        return JsonResponse(res, status=200)
+    else:
+        return JsonResponse(status=404)
