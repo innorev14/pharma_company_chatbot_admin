dfaa0504a0dc8932a6429868d89745f6f8763010
innorev14 <tokyo3@netsgo.com>
2021-04-26 10:59:22 +0900

    feat: kakao url 추가

--- kakao/urls.py
+++ kakao/urls.py
@@ -5,5 +5,11 @@ from .views import *
 urlpatterns = [
     path('keyboard/', keyboard, name='keyboard'),
     path('keyboard2/', keyboard2, name='keyboard2'),
-    path('medicine/', medicine, name='medicine')
+    path('auth/', auth, name='auth'),
+    path('valid/', validation, name='validation'),
+    path('welcome/', welcome, name='welcome'),
+    path('medicine/', medicine, name='medicine'),
+    path('prod_info/', prod_info, name='prod_info'),
+    path('insu_info/', insu_info, name='insu_info'),
+    path('detail_point/', detail_point, name='detail-_point'),
 ]
