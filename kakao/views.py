2ac2e6e011066ec5f8f7b7b608862a8bd2d23a2c
천정환 <innorev@cheonjeonghwan-ui-MacBookPro.local>
2021-05-04 16:24:26 +0900

    fix: test 이미지 주소수정

--- kakao/views.py
+++ kakao/views.py
@@ -1131,7 +1131,10 @@ def detail_point_test(request):
                                     "thumbnail": {
                                         "imageUrl": "https://static.wixstatic.com/media/b647b8_8f92c6051fc84b01b4a8d31b24cacc26~mv2.png/v1/fill/w_1068,h_1436,al_c/"
                                                     + parse.quote("플루카졸") + ".png",
-                                        "link": medicine_info.detail_url
+                                        "link": "https://static.wixstatic.com/media/b647b8_8f92c6051fc84b01b4a8d31b24cacc26~mv2.png/v1/fill/w_1068,h_1436,al_c/"
+                                                    + parse.quote("플루카졸") + ".png",
+                                        "width": 1068,
+                                        "height": 1436,
                                     },
                                 },
                             }
